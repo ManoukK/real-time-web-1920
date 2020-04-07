eb76d6c9d8daffb863d44f22f5dfd72ade4da583
ManoukK <manouk.kappe21@gmail.com>
2020-04-07 15:42:32 +0200

    unieke feature (in de server)

--- public/js/main.js
+++ public/js/main.js
@@ -1,61 +1,113 @@
-$(function () {
-    const socket = io();
-    $('form').submit(function(e){
-      e.preventDefault(); // prevents page reloading
-      socket.emit('chat message', $('#m').val());
-      $('#m').val('');
-      return false;
-    });
-    socket.on('chat message', function(msg){
-      $('#messages').append($('<li>').text(msg));
-    });
-  });
-
 const socket = io();
 
-console.log("hi test");
+// $(function () {
+//     const socket = io();
+//     $('form').submit(function(e){
+//       e.preventDefault(); // prevents page reloading
+//       socket.emit("chat message", document.querySelector("#m").value);
+//       $('#m').val('');
+//       return false;
+//     });
+//     socket.on('chat message', function(msg){
+//     verbsChanger();
+//       $('#messages').append($('<li>').text(msg));
+//     });
+//   });
 
-const verbs = [
-    "beginnen",
-    "denken",
-    "douchen",
-    "eten",
-    "lopen",
-    "studeren",
-    "coderen",
-    "vallen",
-    "vliegen",
-    "werken",
-    "zwemmen",
-    "reizen",
-    "winnen",
-    "koken",
-    "springen",
-    "sturen",
-    "spelen",
-    "slapen",
-    "praten",
-    "lezen",
-    "wassen",
-    "bijten",
-    "bouwen",
-    "typen",
-];
-
-const test1 = verbs.includes("hi");
-const test2 = verbs.includes("typen");
-
-console.log("Deze array bevat het woord hoi", test1);
-console.log("Deze array bevat het woord typen", test2);
-
+// const verbs = [
+//     "beginnen", "denken", "douchen", "eten", "lopen", "studeren", 
+//     "coderen", "vallen", "vliegen", "werken", "zwemmen", "reizen",
+//     "winnen", "koken", "springen", "sturen", "spelen", "slapen",
+//     "praten", "lezen", "wassen", "bijten", "bouwen", "typen",
+//     "gamen", "niezen", "ademen",
+// ];
 
 socket.on('connect', function(socket){
-    console.log("connection to server made");
-    verbsChanger();
+
 });
 
-function verbsChanger(){
-    console.log("Deze function doet het!");
-    const lastMessage = document.getElementById('messages').lastChild;
-    console.log(lastMessage);
+//naam veranderen
+const inputForm = document.getElementById("messageForm");
+const inputField = document.getElementById("m");
+console.log(inputForm);
+
+inputForm.addEventListener('submit', inputText);
+
+function inputText(event){
+    // console.log(event);
+    event.preventDefault();
+    console.log(inputField.value);
+
+    // verbsChanger(inputField.value);
+    // const newMessage = verbsChanger(inputField.value);
+    // console.log(newMessage);
+
+    //emit stuurt naar de server
+    socket.emit("chat message", inputField.value);
+
+    //luistert naar de server (berichten)
+    socket.on('chat message', function(msg){
+        const messages = document.getElementById("messages");
+        messages.insertAdjacentHTML("beforeend", `<li>${msg}</li>`);
+
+        // $('#messages').append($('<li>').text(msg));
+        });
+
+    // socket.on('chat message', function(socket){
+
+    // });
 }
+
+// function verbsChanger(lastMessage){
+//     // console.log("Deze function doet het!");
+//     //const lastMessage = document.getElementById('messages').lastChild;
+
+//     // laatste bericht die is gestuurd (nog uit de chat halen)
+//     //const lastMessage = "Vanaf vandaag ben ik gestopt met gamen";
+//     console.log(lastMessage);
+
+//     //bericht opsplitsen in een array
+//     const splitLastMessage = lastMessage.split(" ");
+
+//     //checken of er waardes overeen komen tussen 2 arrays
+//     //https://stackoverflow.com/questions/1187518/how-to-get-the-difference-between-two-arrays-in-javascript
+//     const intersection = splitLastMessage.filter(x => verbs.includes(x));
+//     console.log("Dit woord is eruit gefilterd:", intersection);
+
+    
+//     //https://www.geeksforgeeks.org/check-if-an-array-is-empty-or-not-in-javascript/
+//     if (Array.isArray(intersection) && intersection.length){ 
+
+//         //random woord kiezen uit de array van werkwoorden
+//         //https://stackoverflow.com/questions/1516695/js-math-random-for-array
+//         const randomVerb = verbs[Math.floor(verbs.length * Math.random())];
+//         console.log("een random werkwoord", randomVerb);
+
+//         //Uitgekozen woord omzetten naar een string
+//         const verbToString = intersection.toString();    
+//         console.log("Dit woord is eruit gefilterd:", verbToString);
+
+//         //De index van het uitgekozen woord achterhalen
+//         const indexOfOldVerb = splitLastMessage.indexOf(verbToString);
+//         console.log("index van het werkwoord in de oorspronkelijke zin", indexOfOldVerb);
+
+//         //het nieuwe random woord op de plek zetten van het oude werkwoord doormiddel van een index nummer
+//         const newArrayWithRandomVerb = splitLastMessage[indexOfOldVerb] = randomVerb;
+//         console.log("oude werkwoord vervangen voor het nieuwe werkwoord:", newArrayWithRandomVerb);
+
+//         //de array weer aan elkaar plakken zodat het een zin word
+//         const messageToString = splitLastMessage.join(" "); 
+
+//         console.log(newArrayWithRandomVerb);
+//         console.log(splitLastMessage);
+//         console.log(messageToString);
+
+
+//         return messageToString;
+//         //lastMessage.innerHTML = messageToString;
+
+//     } else { 
+//             output = false; 
+//             console.log(output);
+//     };
+// };
--- server.js
+++ server.js
@@ -14,14 +14,79 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
 
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-        console.log('message: ' + msg);
+        
+        const newMessage = verbsChanger(msg);
+
+        io.emit('chat message', newMessage);
+        console.log('message: ' + newMessage);
       });
 
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
 });
+
+function verbsChanger(lastMessage){
+    const verbs = [
+        "beginnen", "denken", "douchen", "eten", "lopen", "studeren", 
+        "coderen", "vallen", "vliegen", "werken", "zwemmen", "reizen",
+        "winnen", "koken", "springen", "sturen", "spelen", "slapen",
+        "praten", "lezen", "wassen", "bijten", "bouwen", "typen",
+        "gamen", "niezen", "ademen",
+    ];
+    // console.log("Deze function doet het!");
+    //const lastMessage = document.getElementById('messages').lastChild;
+
+    // laatste bericht die is gestuurd (nog uit de chat halen)
+    //const lastMessage = "Vanaf vandaag ben ik gestopt met gamen";
+    console.log(lastMessage);
+
+    //bericht opsplitsen in een array
+    const splitLastMessage = lastMessage.split(" ");
+
+    //checken of er waardes overeen komen tussen 2 arrays
+    //https://stackoverflow.com/questions/1187518/how-to-get-the-difference-between-two-arrays-in-javascript
+    const intersection = splitLastMessage.filter(x => verbs.includes(x));
+    console.log("Dit woord is eruit gefilterd:", intersection);
+
+    
+    //https://www.geeksforgeeks.org/check-if-an-array-is-empty-or-not-in-javascript/
+    if (Array.isArray(intersection) && intersection.length){ 
+
+        //random woord kiezen uit de array van werkwoorden
+        //https://stackoverflow.com/questions/1516695/js-math-random-for-array
+        const randomVerb = verbs[Math.floor(verbs.length * Math.random())];
+        console.log("een random werkwoord", randomVerb);
+
+        //Uitgekozen woord omzetten naar een string
+        const verbToString = intersection.toString();    
+        console.log("Dit woord is eruit gefilterd:", verbToString);
+
+        //De index van het uitgekozen woord achterhalen
+        const indexOfOldVerb = splitLastMessage.indexOf(verbToString);
+        console.log("index van het werkwoord in de oorspronkelijke zin", indexOfOldVerb);
+
+        //het nieuwe random woord op de plek zetten van het oude werkwoord doormiddel van een index nummer
+        const newArrayWithRandomVerb = splitLastMessage[indexOfOldVerb] = randomVerb;
+        console.log("oude werkwoord vervangen voor het nieuwe werkwoord:", newArrayWithRandomVerb);
+
+        //de array weer aan elkaar plakken zodat het een zin word
+        const messageToString = splitLastMessage.join(" "); 
+
+        console.log(newArrayWithRandomVerb);
+        console.log(splitLastMessage);
+        console.log(messageToString);
+
+
+        return messageToString;
+        //lastMessage.innerHTML = messageToString;
+
+    } else { 
+            output = false; 
+            console.log(output);
+            return lastMessage;
+    };
+};
 
 http.listen(port, function(){
     console.log('listening on *:3000');
